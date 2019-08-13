d5aa16dd4777b05a30c581667166f0d4c0b4f797
jjuancruzrey <jjuancruzrey@gmail.com>
2019-08-13 04:30:14 -0300

    create service,add bootstrap & create cliente

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  { id: 7, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2017-11-16' },
+  { id: 8, nombre: 'Bombasto', apellido: 'Vlissides', email: 'bombasto.vlissides@gmail.com', createAt: '2017-11-17' },
+  { id: 9, nombre: 'Dr James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2017-11-18' },
+  { id: 10, nombre: 'Magma', apellido: 'Lee', email: 'magma.lee@gmail.com', createAt: '2017-11-19' },
+  { id: 11, nombre: 'Tornado', apellido: 'Roe', email: 'tornado.roe@gmail.com', createAt: '2017-11-20' }
+];
